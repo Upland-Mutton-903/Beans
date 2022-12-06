b25cfe32fc5ba82190449ffa40846aeb02a0c64d
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2022-12-06 08:03:14 -0600

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function () {var a = document.createElement('script');a.src = 'https://cdn.jsdelivr.net/gh/FogNetwork/Ingot@latest/ingot.min.js';document.body.appendChild(a);}())
+javascript:(function () {var a = document.createElement('script');a.src = 'https://raw.githubusercontent.com/Upland-Mutton-903/Beans/main/ingot.min.js';document.body.appendChild(a);}())
